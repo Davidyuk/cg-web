895861b1396203cc572b72f9a78328a78f67c0a5
Denis <denis_davidyuk@hotmail.com>
2016-08-08 17:26:11 +1000

    menu: Disable item after click

--- client/assets/js/models/menu.js
+++ client/assets/js/models/menu.js
@@ -2,6 +2,12 @@ var Backbone = require('backbone');
 
 
 module.exports = Backbone.Model.extend({
+  defaults: {
+    active: false,
+    here: false,
+    icon: null
+  },
+
   initialize: function(attributes, options) {
     this.listenTo(options.vent, 'route', this.onRoute);
   },
@@ -9,6 +15,7 @@ module.exports = Backbone.Model.extend({
   onRoute: function() {
     var href = this.get('href');
     this.set('active', href.length > 1 && !location.pathname.lastIndexOf(href, 0));
+    this.set('here', href == location.pathname);
   },
 
   sync: function() { return false; }
